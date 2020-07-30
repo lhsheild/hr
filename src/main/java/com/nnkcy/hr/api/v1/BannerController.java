120f56cd0e825cd858b84173b825308320119fb8
lhsheildg <lhsheild@yahoo.com>
2020-07-31 00:54:41 +0800

    OCP(开闭原则)

--- src/main/java/com/nnkcy/hr/api/v1/BannerController.java
+++ src/main/java/com/nnkcy/hr/api/v1/BannerController.java
@@ -1,21 +1,20 @@
 package com.nnkcy.hr.api.v1;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Controller
+@RestController
+@RequestMapping("/v1/banner")
 public class BannerController {
     // MVC
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
+//    @RequestMapping(value = "/test", method = {RequestMethod.GET})
+//    @ResponseBody
+    @GetMapping()
+    public String banner(){
 //        response.getWriter().write("temp");
-        return "test, 七七七七七";
+        return "banner, 无所谓";
     }
 }
